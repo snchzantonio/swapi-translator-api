fff27b560c8f20f71a3717397504e6fb96ff901b
antonio sanchez (home) <snchzantonio@gmail.com>
2021-06-21 15:56:36 -0500

    agregar ejemplo de integracion a la DB

---
+++ things/create.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async (event, context) => {
+  const timestamp = new Date().getTime();
+  let response;
+  console.log(event);
+  
+  try {
+
+    const data = JSON.parse(event.body);
+
+    if (typeof data.text !== 'string') {
+      const miError = new Error('No se pudo agregar');
+      miError.statusCode = 400;
+      throw miError;
+    }
+
+    const params = {
+      TableName: "things",
+      Item: {
+        id: uuid.v1(),
+        text: data.text,
+        createdAt: timestamp,
+        updatedAt: timestamp,
+      },
+    };
+
+    const putResult = await dynamoDb.put(params).promise();
+    
+    console.log("PUT RESULT");
+    console.log(putResult);
+
+     response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        ok: true,
+        error: false,
+        data: params.Item
+
+      })
+    };
+    
+    console.log("RESPUESTA");
+    console.log(response);
+
+  } catch (error) {
+     console.log("ERROR");
+    console.log(error);
+    
+    response = {
+      statusCode: error.statusCode || 501,
+      body: JSON.stringify(error.message)
+    };
+  }
+    
+  console.log("DEVOLVIENDO RESPUESTA");
+  console.log(response);
+
+  return response;
+
+};
+
+
---
+++ things/list.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handler = async (event, context) => {
+  const params = {
+    TableName: 'things',
+  };
+  const listResults = await dynamoDb.scan(params).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(listResults.Items)
+  }
+
+
+};
